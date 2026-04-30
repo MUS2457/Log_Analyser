6ab52f9e6d88d05eee5433b1d9f7ce3e61881fd8
Amine Mustapha <mustaphaamine54@gmail.com>
2026-04-30 20:14:35 +0900

    create fc from_dic to object

--- CORE/module.py
+++ CORE/module.py
@@ -6,4 +6,8 @@ class LogEntry :
         self.message = message
         self.raw = raw
 
+    @classmethod
+    def from_dict(cls, log_info):
+        return cls(log_info['timestamp'], log_info['level'], log_info['message'], log_info['raw'])
+
 
