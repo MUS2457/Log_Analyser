a2992df0a566491d313a6a121967e841a188d65c
Amine Mustapha <mustaphaamine54@gmail.com>
2026-05-09 12:25:31 +0900

    search by keyword (sql), view_summaries_based_on_date (json) has benn added

--- UTILS/tools.py
+++ UTILS/tools.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from CORE.module import LogEntry
 import re
+from DATABASE import summary_json
 
 def get_results(rows):
     results = {}
@@ -138,4 +139,79 @@ def search_by_date_range(conn):
             print(f"{dt} : {info}")
 
         return results
+
+def search_by_keyword(conn):
+        cursor = conn.cursor()
+
+        while True:
+            print("SEARCH BY KEYWORD TOOL")
+            user_keyword = input("Please enter the keyword of log or 'exit' to quit: ").strip()
+
+            if user_keyword.lower() == "exit":
+                print("Quitting...")
+                break
+
+            cursor.execute("""SELECT log.created_at,
+                                     log.raw_message,
+                                     log_info.timestamp,
+                                     log_info.level,
+                                     log_info.message
+                                     FROM log 
+                                     JOIN log_info ON log.id = log_info.log_id
+                                     WHERE LOWER(raw_message) LIKE ?
+            """, (("%" + user_keyword.lower() + "%"),
+            ))
+
+            rows = cursor.fetchall()
+
+            if not rows:
+                print("No entries found for that keyword.")
+                continue
+
+            results = get_results(rows)
+
+            print(f"\nRESULTS FOR KEYWORD: {user_keyword}, {len(results)} entries found.")
+            for dt, info in results.items():
+                print(f"{dt} : {info}")
+
+            return results
+
+
+def view_summaries_based_on_date():
+    data = summary_json.load_summary()
+
+    if not data:
+        print("No data exists , run the program at least once.")
+        return {}
+
+    while True:
+        pattern = r"^\d{4}-\d{2}-\d{2}$"
+        print("VIEW SUMMARY BY DATE")
+        user_date = input("Please enter the date of log or 'exit' to quit: ").strip()
+
+        if user_date.lower() == "exit":
+            print("Quitting...")
+            break
+
+        elif not re.match(pattern, user_date):
+            print("Invalid date format.")
+            continue
+
+        results = {}
+
+        for date, summaries in data.items():
+            if date.startswith(user_date):
+                results[date] = summaries
+
+        if not results:
+            print("No summaries found for that date.")
+            continue
+
+        print(f"\nRESULTS FOR DATE: {user_date} ,{len(results)} summaries found.")
+        for date, summaries in results.items():
+            print(f"{date} : {summaries}")
+
+        return results
+
+
 
