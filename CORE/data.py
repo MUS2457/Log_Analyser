7794270383083efae8f0feeaa7ce37f6770b516e
Amine Mustapha <mustaphaamine54@gmail.com>
2026-05-09 17:10:20 +0900

    refactor: update define_data to use streaming file reader for memory safety

--- CORE/data.py
+++ CORE/data.py
@@ -1,20 +1,11 @@
-import parser
-import module
+from CORE import  parser, reader
 
-def define_data(path_log):
+def define_data(file_path):
     entries = []
-
-    try:
-        with open(path_log, "r") as logs:
-            for line in logs:
-                entry_dict = parser.parse_line(line)
-
-                if entry_dict:
-                    entries.append(module.LogEntry.from_dict(entry_dict))
-
-        return entries
-
-    except (FileNotFoundError, PermissionError, UnicodeDecodeError, OSError):
-        return None
+    for line in reader.read_file(file_path):
+        parsed = parser.parse_line(line)
+        if parsed:
+            entries.append(parsed)
+    return entries
 
 
